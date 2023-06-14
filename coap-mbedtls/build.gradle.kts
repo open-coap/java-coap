4cb895818752f325ac991454ebe185d42fb31896
Topias Jokiniemi <topias.jokiniemi@nordicsemi.no>
2023-06-14 14:44:57 +0300

    update mbedtls-library to newest

--- coap-mbedtls/build.gradle.kts
+++ coap-mbedtls/build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022-2023 java-coap contributors (https://github.com/open-coap/java-coap)
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 plugins {
     id("java-library")
 }
@@ -6,10 +22,10 @@ description = "coap-mbedtls"
 
 dependencies {
     api(project(":coap-core"))
-    api("io.github.open-coap:kotlin-mbedtls:1.14.3")
+    api("io.github.open-coap:kotlin-mbedtls:1.15.0")
 
     testImplementation(project(":coap-netty"))
-    testImplementation("io.github.open-coap:kotlin-mbedtls-netty:1.14.3")
+    testImplementation("io.github.open-coap:kotlin-mbedtls-netty:1.15.0")
 
     testImplementation(testFixtures(project(":coap-core")))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
