00feadc5bc6878657a15444b13f77801bea0f1b4
Szymon Sasin <szymon.sasin@gmail.com>
2024-07-10 21:07:53 +0300

    GH-86 Fix warning: 'Class is exposed outside of its visibility scope'

--- coap-tcp/src/main/java/com/mbed/coap/packet/SignallingHeaderOptions.java
+++ coap-tcp/src/main/java/com/mbed/coap/packet/SignallingHeaderOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 java-coap contributors (https://github.com/open-coap/java-coap)
+ * Copyright (C) 2022-2024 java-coap contributors (https://github.com/open-coap/java-coap)
  * Copyright (C) 2011-2021 ARM Limited. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -54,7 +54,7 @@ public class SignallingHeaderOptions extends HeaderOptions {
     }
 
     @Override
-    protected List<RawOption> getRawOptions() {
+    List<RawOption> getRawOptions() {
         List<RawOption> l = super.getRawOptions();
         if (signallingOption2 != null) {
             l.add(new RawOption(SIGN_OPTION_2, signallingOption2));
