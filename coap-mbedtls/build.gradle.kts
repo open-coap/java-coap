57f436b982588d966bcbb8db1b1906c9b9efc7c3
Szymon Sasin <szymon.sasin@gmail.com>
2026-02-18 10:47:16 +0200

    ci: run on windows

--- coap-mbedtls/build.gradle.kts
+++ coap-mbedtls/build.gradle.kts
@@ -17,3 +17,23 @@ dependencies {
     testImplementation("ch.qos.logback:logback-classic:1.5.18")
     testImplementation("org.awaitility:awaitility:4.3.0")
 }
+
+
+tasks {
+
+    register<Copy>("extractNativeLibs") {
+        from(configurations.runtimeClasspath.get().filter { it.name.contains("mbedtls-lib") }.map { zipTree(it) })
+        include("win32-x86-64/**")
+        into(layout.buildDirectory.dir("native-libs"))
+    }
+
+
+    test {
+        if (System.getProperty("os.name").lowercase().contains("windows")) {
+            // On Windows, JNA cannot automatically load native libraries from JAR when they have dependencies on each other,
+            // so we extract them and set the path explicitly.
+            dependsOn("extractNativeLibs")
+            systemProperty("jna.library.path", layout.buildDirectory.dir("native-libs/win32-x86-64").get().asFile.absolutePath)
+        }
+    }
+}
